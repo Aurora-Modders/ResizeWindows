e0d6a9de60c1646aec52fc74ed9844f313f0f795
Edouard Poitras <eddie@quantumcyberdefence.com>
2021-05-11 21:45:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# ResizeWindows
+
+A simple patch to allow window resizing in Aurora.
+
+![ResizeWindows](/ResizeWindows.png?raw=true)
---
+++ ResizeWindows.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+using HarmonyLib;
+
+namespace ResizeWindows
+{
+    public class ResizeWindows : AuroraPatch.Patch
+    {
+        public override string Description => "Allow for resizing windows.";
+
+        protected override void Loaded(Harmony harmony)
+        {
+            LogInfo("Loading ResizeWindows...");
+            HarmonyMethod formConstructorPostfixMethod = new HarmonyMethod(GetType().GetMethod("FormConstructorPostfix", AccessTools.all));
+            foreach (var form in AuroraAssembly.GetTypes().Where(t => typeof(Form).IsAssignableFrom(t)))
+            {
+                try
+                {
+                    foreach (var ctor in form.GetConstructors())
+                    {
+                        harmony.Patch(ctor, postfix: formConstructorPostfixMethod);
+                    }
+                }
+                catch (Exception e)
+                {
+                    LogError($"ResizeWindows failed to patch Form constructor {form.Name}, exception: {e}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Harmony method to postfix the Form constructor in order to add our custom HandleCreated callback.
+        /// </summary>
+        /// <param name="__instance"></param>
+        private static void FormConstructorPostfix(Form __instance)
+        {
+            __instance.HandleCreated += CustomHandleCreated;
+        }
+
+        /// <summary>
+        /// Our custom handle created callback will set our sizable property.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void CustomHandleCreated(Object sender, EventArgs e)
+        {
+            Form form = sender as Form;
+            form.FormBorderStyle = FormBorderStyle.Sizable;
+            form.AutoScroll = true;
+        }
+    }
+}
